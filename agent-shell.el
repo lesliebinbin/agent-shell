39b201b4f6ad483d37fd60d53c9fe6e0e73d3c2f
lesliebinbin <leslie.huang@basf.com>
2026-04-21 22:49:59 +0800

    Fix var missing

--- agent-shell.el
+++ agent-shell.el
@@ -9,6 +9,9 @@
 
 (defconst agent-shell--version "0.50.1")
 
+
+(defvar shell-maker--prompt-rear-nonsticky t
+  "Compatibility shim for shell-maker 0.5.0+.")
 ;; This package is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 3, or (at your option)
